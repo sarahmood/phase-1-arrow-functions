1ffece706f2334ba8d19d820e0bfc6c7cb617c49
sarahmood <sarahamoud009@gmail.com>
2022-04-25 23:41:08 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+// Function expression
+const divide = function (){
+    return 2000 /100;
+}
+
+console.log( divide() );
+
+// Arrow function
+
+const square = x => x ** x ;
+console.log ( square(2) );
+
+const add = (parone, partwo) => parone + partwo;
+console.log ( (add(3,4)) );
